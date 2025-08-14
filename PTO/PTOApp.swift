2697eb6c339883cf0aaaeea5148eea9c51b2f7fa
Perry <nuperry2011@gmail.com>
2025-08-13 19:30:58 -0700

    Initial Commit

---
+++ PTO/PTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  PTOApp.swift
+//  PTO
+//
+//  Created by Nicholas Perry on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct PTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
