5d64871e0f7d2f7cf897b41740dbe40e2f799bc2
Perry <nuperry2011@gmail.com>
2025-08-20 18:37:23 -0700

    adding an done button

--- PTO/Views/SettingsView.swift
+++ PTO/Views/SettingsView.swift
@@ -53,6 +53,14 @@ struct SettingsView: View {
                             }
                             .accessibilityLabel("Hourly wage input")
                             .accessibilityValue(tempWage)
+                            .toolbar {
+                                ToolbarItemGroup(placement: .keyboard) {
+                                    Spacer()
+                                    Button("Done") {
+                                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                                    }
+                                }
+                            }
                         
                         Text(settings.currencySymbol + "/hr")
                             .corporateSubheadStyle()
